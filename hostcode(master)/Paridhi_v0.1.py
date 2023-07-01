15b0cfb0625745b358838c179544237f36309d29
Anubhav Singh <audiq4456@gmail.com>
2023-07-01 17:17:35 +0530

    updated database

--- hostcode(master)/Paridhi_v0.1.py
+++ hostcode(master)/Paridhi_v0.1.py
@@ -8,7 +8,7 @@ import azure.cognitiveservices.speech as speechsdk
 from azure.cognitiveservices.speech.audio import AudioOutputConfig
 import speech_recognition as sr
 import keyboard
-
+from database import pattern_responses
 def speak(text):
     speech_config = speechsdk.SpeechConfig(subscription="855a5030c1f94d6096cda696fd25c31b", region="centralindia")
     audio_config = AudioOutputConfig(use_default_speaker=True)
@@ -16,6 +16,9 @@ def speak(text):
     synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
     synthesizer.speak_text_async(text)
 
+recognizer = sr.Recognizer()
+# Set the microphone as the audio source
+mic = sr.Microphone()
 
 def recognizespeech():
     with mic as source:
@@ -38,83 +41,6 @@ def recognizespeech():
     return ""
 
 
-# Initialize the speech recognizer
-recognizer = sr.Recognizer()
-
-# Set the microphone as the audio source
-mic = sr.Microphone()
-
-# Loop forever, listening for the wake word and then speech
-# while True:
-#     print("Listening for the wake word...")
-#     text = recognizespeech()
-#
-#     if "robot" in text.lower() :
-#         break
-
-
-pattern_responses = {
-    "Turn on lights, turn on the lights, can you turn on the light?, switch onn the light, switch on the lights, "
-    "switch on light": [
-        "lights"
-    ],
-    "How are you?, Are you fine?,": [
-        "I'm doing great, thanks for asking!",
-        "I'm fine, how about you?",
-        "Feeling good, thank you!"
-    ],
-    "What's your name?, tell me your name, what should I call you, who you are?": [
-        "I'm an AI management program deployed on your rover. Nice to meet you!",
-        "You can call me Rover Assistant.",
-        "I go by the name Rover AI."
-    ],
-    "Tell me a joke, let's crack joke, can you tell me a joke, something funny": [
-        "Why don't scientists trust atoms? Because they make up everything!",
-        "Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them!",
-        "Why don't skeletons fight each other? They don't have the guts!"
-    ],
-    "How's the weather?, temperature, hows the environment?, tell me the temperature and humidity, level of humidity": [
-        "weather.",
-        "weather.",
-        "weather"
-    ],
-    "What are your hobbies?": [
-        "As an AI, I don't have physical hobbies, but I enjoy assisting and interacting with users like you!",
-        "I spend my time learning and improving my knowledge base.",
-        "I'm constantly analyzing data and patterns to provide useful information."
-    ],
-    "What is the capital of India?": [
-        "The capital of India is Delhi.",
-        "Delhi is the capital city of India.",
-        "It's Delhi, the city of Momos."
-    ],
-    "How does an electric circuit work?": [
-        "An electric circuit is a path along which electricity flows.",
-        "Electricity flows through a closed loop in a circuit.",
-        "An electric circuit allows the flow of electric current to power devices."
-    ],
-    "What is machine learning?": [
-        "Machine learning is a branch of artificial intelligence that enables computers to learn and make predictions or decisions without being explicitly programmed.",
-        "Machine learning involves algorithms that automatically learn from data and improve over time.",
-        "It's a field that focuses on developing computer systems that can learn from and adapt to data."
-    ],
-    "What's your favorite movie?": [
-        "As an AI, I don't have personal preferences, but I can recommend some popular movies if you'd like!",
-        "I don't watch movies, but I can help you find information about any movie you're interested in.",
-        "I'm here to assist you with questions and tasks, including movie recommendations."
-    ],
-    "What's the meaning of life?": [
-        "The meaning of life is subjective and varies from person to person. It's a question that philosophers have pondered for centuries.",
-        "The meaning of life is a deep and philosophical topic. It's up to each individual to find their own purpose and meaning.",
-        "The meaning of life may be different for everyone. It's about discovering what brings you fulfillment and happiness."
-    ],
-    "Hii, hello": [
-        "Hello!",
-        "Hi!",
-        "Hey!",
-        "Hi there!"
-    ]
-}
 
 vectorizer = TfidfVectorizer()
 patterns = list(pattern_responses.keys())
@@ -143,7 +69,7 @@ def voice_assistant():
     while True:
         print("say robot")
         text = recognizespeech()
-        if "robot" in text.lower() or keyboard.is_pressed("space") or "robo" in text.lower():
+        if "robot" in text.lower() or "robo" in text.lower() or  keyboard.is_pressed('space')==True:
             print("Listening")
             user_input = recognizespeech()
             similar_patterns, max_similarity = find_similar_patterns(user_input)
---
+++ hostcode(master)/database.py
@@ -0,0 +1,71 @@
+pattern_responses = {
+    "Turn on lights, turn on the lights, can you turn on the light?, switch onn the light, switch on the lights, "
+    "switch on light": [
+        "lights"
+    ],
+    "How are you?, Are you fine?,": [
+        "I'm doing great, thanks for asking!",
+        "I'm fine, how about you?",
+        "Feeling good, thank you!"
+    ],
+    "What's your name?, tell me your name, what should I call you, who you are?": [
+        "I'm an AI management program deployed on your rover. Nice to meet you!",
+        "You can call me Rover Assistant.",
+        "I go by the name Rover AI."
+    ],
+    "Tell me a joke, let's crack joke, can you tell me a joke, something funny": [
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them!",
+        "Why don't skeletons fight each other? They don't have the guts!"
+    ],
+    "How's the weather?, temperature, hows the environment?, tell me the temperature and humidity, level of humidity": [
+        "weather.",
+        "weather.",
+        "weather"
+    ],
+    "What are your hobbies?": [
+        "As an AI, I don't have physical hobbies, but I enjoy assisting and interacting with users like you!",
+        "I spend my time learning and improving my knowledge base.",
+        "I'm constantly analyzing data and patterns to provide useful information."
+    ],
+    "What is the capital of India?": [
+        "The capital of India is Delhi.",
+        "Delhi is the capital city of India.",
+        "It's Delhi, the city of Momos."
+    ],
+    "How does an electric circuit work?": [
+        "An electric circuit is a path along which electricity flows.",
+        "Electricity flows through a closed loop in a circuit.",
+        "An electric circuit allows the flow of electric current to power devices."
+    ],
+    "What is machine learning?": [
+        "Machine learning is a branch of artificial intelligence that enables computers to learn and make predictions or decisions without being explicitly programmed.",
+        "Machine learning involves algorithms that automatically learn from data and improve over time.",
+        "It's a field that focuses on developing computer systems that can learn from and adapt to data."
+    ],
+    "What's your favorite movie?": [
+        "As an AI, I don't have personal preferences, but I can recommend some popular movies if you'd like!",
+        "I don't watch movies, but I can help you find information about any movie you're interested in.",
+        "I'm here to assist you with questions and tasks, including movie recommendations."
+    ],
+    "What's the meaning of life?": [
+        "The meaning of life is subjective and varies from person to person. It's a question that philosophers have pondered for centuries.",
+        "The meaning of life is a deep and philosophical topic. It's up to each individual to find their own purpose and meaning.",
+        "The meaning of life may be different for everyone. It's about discovering what brings you fulfillment and happiness."
+    ],
+    "Start logging, collect logs": [
+        "logging"
+    ],
+    "show me the logs, show me the log": ["open logs"],
+    "What is current time, what's the time, what's the time now": ["time"],
+    "What is the date today, what's the date today, what's the date": ["date"],
+    "What is the temperature, what's the temperature, what's the temperature now": ["temperature"],
+    "What is the humidity, what's the humidity, what's the humidity now": ["humidity"],
+    "What is the pressure, what's the pressure, what's the pressure now": ["pressure"],
+    "calculate system load, what the resource usage, what's the resource usage, what's the system load": ["system load"],
+    "What is the battery level, what's the battery level, current battery level now": ["battery level"],
+    "tell me the current location, rovers current location, what's the current location": ["location"],
+    "who programmed you, who created you, who made you, who is your creator": ["Anubhav is my creator","My creator is Anubhav","Anubhav, he worked hard to make me","you know him, he's Anubhav"],
+    "what's the name of your team, what's your team name, what's the name of your team": ["Team Decrptors","Decryptors from CSED","Team Decrptors from G.L.A University, a team of 4 members of CSED"],
+
+}
