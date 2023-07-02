81089158827cb15bc360705f50734bee593e1f8a
Anubhav Singh <audiq4456@gmail.com>
2023-07-02 19:00:53 +0530

    Added GUI and Voice Functionality.

--- hostcode(master)/Paridhi_v0.1.py
+++ hostcode(master)/Paridhi_v0.1.py
@@ -1,3 +1,7 @@
+import sys
+from PySide6.QtCore import Qt, QThread, Signal
+from PySide6.QtGui import QColor, QLinearGradient, QPainter, QPalette
+from PySide6.QtWidgets import QApplication, QMainWindow, QTextEdit, QPushButton, QVBoxLayout, QWidget
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -9,6 +13,7 @@ from azure.cognitiveservices.speech.audio import AudioOutputConfig
 import speech_recognition as sr
 import keyboard
 from database import pattern_responses
+
 def speak(text):
     speech_config = speechsdk.SpeechConfig(subscription="855a5030c1f94d6096cda696fd25c31b", region="centralindia")
     audio_config = AudioOutputConfig(use_default_speaker=True)
@@ -31,15 +36,16 @@ def recognizespeech():
         # print("You said: " + text)
         if not text:
             print("No speech detected. Please say it again clearly.")
-        return text
+            return ""
+        else:
+            return text
 
     except sr.UnknownValueError:
         print("Sorry, I could not understand what you said. Please say it again clearly.")
+        return ""
     except sr.RequestError as e:
         print("Error occurred during speech recognition: {0}".format(e))
-
-    return ""
-
+        return ""
 
 
 vectorizer = TfidfVectorizer()
@@ -65,15 +71,105 @@ def find_similar_patterns(user_input):
     return similar_patterns, max_similarity
 
 
-def voice_assistant():
-    while True:
-        print("say robot")
-        text = recognizespeech()
-        if "robot" in text.lower() or "robo" in text.lower() or  keyboard.is_pressed('space')==True:
-            print("Listening")
-            user_input = recognizespeech()
+class SpeechRecognitionThread(QThread):
+    speech_input_signal = Signal(str)
+    speech_recognized_signal = Signal(str)
+
+    def run(self):
+        while True:
+            print("say robot")
+            text = recognizespeech()
+            if "robot" in text.lower() or "robo" in text.lower() or keyboard.is_pressed('space') == True:
+                print("Listening")
+                user_input = recognizespeech()
+                similar_patterns, max_similarity = find_similar_patterns(user_input)
+                print("Similar patterns:", similar_patterns)
+
+                if max_similarity > 0.5:
+                    responses = []
+                    for pattern in similar_patterns:
+                        responses.extend(pattern_responses[pattern])
+
+                    response = random.choice(responses)
+                    print("Assistant:", response)
+                    speak(response)
+
+                    if 'lights' in response:
+                        speak("turning on the lights")
+                        output = subprocess.check_output("python lights.py", shell=True)
+                        output = output.decode("utf-8").strip()
+                        print(output)
+                    elif 'off' in response:
+                        output = subprocess.check_output("python lights.py", shell=True)
+                        output = output.decode("utf-8").strip()
+                        print(output)
+                else:
+                    speak("Can you please repeat?")
+
+    def on_speech_input(self, text):
+        self.speech_input_signal.emit(text)
+
+    def on_speech_recognized(self, text):
+        print("Speech recognized:", text)
+        self.speech_recognized_signal.emit(text)
+class ChatWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Hii, I am Paridhi the Rover's AI, Type 'Voice' to activate voice mode and then say 'Robot' to activate me")
+        self.setGeometry(500, 100, 700, 500)
+
+        # Create a central widget and set it as the main window's central widget
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        # Create a vertical layout for the central widget
+        layout = QVBoxLayout(central_widget)
+
+        # Create a text edit widget to display the conversation
+        self.conversation_text = QTextEdit()
+        layout.addWidget(self.conversation_text)
+
+        # Create a horizontal layout for the text input field and send button
+        input_layout = QVBoxLayout()
+        layout.addLayout(input_layout)
+
+        # Create a text input field for the user to type in
+        self.input_field = QTextEdit()
+        self.input_field.setMaximumHeight(50)
+        input_layout.addWidget(self.input_field)
+
+        # Create a send button to send the user input to the assistant
+        send_button = QPushButton("Send")
+        send_button.clicked.connect(self.send_user_input)
+        input_layout.addWidget(send_button)
+        # Set the background gradient
+        self.set_background_gradient()
+
+        ## Create a speech recognition thread and connect its signals to slots
+        self.recognition_thread = SpeechRecognitionThread()
+        self.recognition_thread.speech_recognized_signal.connect(self.on_speech_recognized)
+        self.recognition_thread.speech_input_signal.connect(self.on_speech_input)
+
+    def on_speech_recognized(self, text):
+        self.conversation_text.append("You: " + text)
+
+    def on_speech_input(self, text):
+        if "robot" in text.lower():
+            self.recognition_thread.start()
+
+    def send_user_input(self):
+        # Get the user input from the text input field
+        user_input = self.input_field.toPlainText()
+
+        # Append the user input to the conversation text and clear the input field
+        self.conversation_text.append("You: " + user_input)
+        self.input_field.clear()
+
+        if "Voice" in user_input:
+            self.recognition_thread.start()
+        else:
+            # Get the assistant's response to the user input
             similar_patterns, max_similarity = find_similar_patterns(user_input)
-            print("Similar patterns:", similar_patterns)
 
             if max_similarity > 0.5:
                 responses = []
@@ -81,7 +177,7 @@ def voice_assistant():
                     responses.extend(pattern_responses[pattern])
 
                 response = random.choice(responses)
-                print("Assistant:", response)
+                self.conversation_text.append("Assistant: " + response)
                 speak(response)
 
                 if 'lights' in response:
@@ -95,7 +191,18 @@ def voice_assistant():
                     print(output)
             else:
                 speak("Sorry, I didn't get that. Can you please repeat?")
-
-
+    def set_background_gradient(self):
+        pal = QPalette()
+        gradient = QLinearGradient(0, 0, 0, 400)
+        gradient.setColorAt(0.0, QColor(255, 255, 255))
+        gradient.setColorAt(1.0, QColor(0, 0, 255))
+        pal.setBrush(QPalette.Window, gradient)
+        self.setPalette(pal)
 if __name__ == '__main__':
-    voice_assistant()
+    # Create the Qt application and the main window
+    app = QApplication(sys.argv)
+    window = ChatWindow()
+    window.show()
+
+    # Run the event loop
+    sys.exit(app.exec())
